74b3bae7fbead4488cf0e2856e7d8abf20f08617
Anton2050 <avvsoft2050@gmail.com>
2023-06-02 19:51:43 +0300

    set correct behavior when back key is pressed on the GameFragment

--- app/src/main/java/com/avv2050soft/sportsquiz/presentation/MainActivity.kt
+++ app/src/main/java/com/avv2050soft/sportsquiz/presentation/MainActivity.kt
@@ -63,8 +63,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        isTicking = false
-        questionNumber = 0
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
